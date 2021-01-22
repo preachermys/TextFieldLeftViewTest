89565d94f2b58e08cf3cd6ef5cba40bd04323d8b
Yulia Milovanova <ymilovanova@actionlearning.ru>
2021-01-22 16:06:48 +0300

    Initial Commit

---
+++ TextFieldLeftViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldLeftViewTest
+//
+//  Created by Preacher on 22.01.2021.
+//  Copyright © 2021 Yulia Milovanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
